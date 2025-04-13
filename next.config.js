cbc6010784b8d402e4fbaf7d16e376d29222c3c2
Stjoo0925 <stjoo0925@gmail.com>
2025-04-13 09:32:59 +0900

    fix : vercel 타임아웃 옵션 개선

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
-    domains: ['avatars.githubusercontent.com', 'cdn.pixabay.com', 'localhost', 'vercel.app'],
+    domains: ['avatars.githubusercontent.com', 'cdn.pixabay.com', 'localhost', 'vercel.app', 'raw.githubusercontent.com'],
     unoptimized: true, // @vercel/og에서 사용할 이미지 최적화 비활성화
   },
   // 서버리스 함수 메모리 제한 증가
   experimental: {
-    serverMemoryLimit: 512 // MB
+    serverMemoryLimit: 512, // MB
+    largePageDataBytes: 512 * 1000, // 512KB -> 내부 props 크기 제한 증가
   },
   // 환경 변수를 클라이언트에 전달
   env: {
     NEXT_PUBLIC_VERCEL_URL: process.env.VERCEL_URL || 'localhost:3000',
     VERCEL_URL: process.env.VERCEL_URL || 'localhost:3000',
+  },
+  // Vercel API 타임아웃 설정
+  serverRuntimeConfig: {
+    maxDuration: 60, // API 요청 최대 시간을 60초로 설정
   },
 };
 
