27762239454ce947960bf23a85c3c300f6fcb1a0
Stjoo0925 <stjoo0925@gmail.com>
2025-04-19 22:49:54 +0900

    feat : meta tag 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,20 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/icons/favicon.ico" />
         <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <link rel="manifest" href="/icons/site.webmanifest" />
+        
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://please-readme.vercel.app/" />
+        <meta property="og:title" content="Please Readme" />
+        <meta property="og:description" content="GitHub 사용자명과 간단한 정보를 입력하면 멋진 GitHub 프로필 이미지를 생성합니다." />
+        <meta property="og:image" content="https://please-readme.vercel.app/images/readme_tn.webp" />
+        
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://please-readme.vercel.app/" />
+        <meta property="twitter:title" content="Please Readme" />
+        <meta property="twitter:description" content="GitHub 사용자명과 간단한 정보를 입력하면 멋진 GitHub 프로필 이미지를 생성합니다." />
+        <meta property="twitter:image" content="https://please-readme.vercel.app/images/readme_tn.webp" />
       </Head>
       <Component {...pageProps} />
     </div>
